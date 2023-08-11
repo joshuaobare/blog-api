423d9cdc28dd9c2e92f228ed48a9246ace6cf17e
joshuaobare <jobare.obare@gmail.com>
2023-08-11 19:59:51 +0300

    imported Bootstrap card component and updated css

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import NavBar from "./components/NavBar.js"
-import Post from "./components/Post.js"
-import uniqid from 'uniqid'
+import "./App.css";
+import { useEffect, useState } from "react";
+import NavBar from "./components/NavBar.js";
+import Post from "./components/Post.js";
+import uniqid from "uniqid";
 
 function App() {
-
   const [posts, setPosts] = useState([]);
 
-  useEffect(()=> {
-    fetchPosts()
-  }, [])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-  const fetchPosts = async() => {
-    const postsResponse = await fetch("http://localhost:3000/posts")    
-    const posts = await postsResponse.json()
-    setPosts(posts)
-
-  }
-
+  const fetchPosts = async () => {
+    const postsResponse = await fetch("http://localhost:3000/posts");
+    const posts = await postsResponse.json();
+    setPosts(posts);
+  };
 
   return (
     <div className="App">
       <NavBar />
-      {
-        posts.map(post => <Post key={uniqid()} post= {post} />)
-      }
+      <h1>All Posts</h1>
+      <div className="posts-grid">
+        {posts.map((post) => (
+          <Post 
+            key={uniqid()} 
+            post={post} 
+          />
+        ))}
+        
+      </div>
     </div>
   );
 }
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,19 @@
-export default function Post(props){
-    return (
-        <div>
-            <h1>{props.post.title}</h1>
-            <p>{props.post.text}</p>
-            <p>{props.post.timestamp}</p>
-            <p>{props.post.authorId}</p>
-        </div>
+import Card from "react-bootstrap/Card";
 
-    )
+export default function Post(props) {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>{props.post.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {props.post.authorId}
+          </Card.Subtitle>
+          <Card.Text>{props.post.text}</Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">{props.post.timestamp}</Card.Link>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
