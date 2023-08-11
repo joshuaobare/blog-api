1ed9f38a872905f5e081aeedc98228214f1e193e
joshuaobare <jobare.obare@gmail.com>
2023-08-11 15:52:20 +0300

    populated models

---
+++ api/models/commentsModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CommentSchema = new Schema({
+  text: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now() },
+  postId: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ api/models/postsModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now() },
+  authorId: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
