5762be55f2d88a878074110560a6366f32596d09
joshuaobare <jobare.obare@gmail.com>
2023-08-11 18:46:11 +0300

    added cors to express app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const posts = useState({});
+
+  useEffect(()=> {
+    fetchPosts()
+  }, [])
+
+  const fetchPosts = async() => {
+    const unparsedPosts = await fetch("http://localhost:3000/posts")
+    console.log(unparsedPosts)
+    const posts = await unparsedPosts.json()
+
+    console.log(posts)
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
