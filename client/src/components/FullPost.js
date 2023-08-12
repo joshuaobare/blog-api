c3692441c7a99d2c004a780ee8edc8ec4898f9e4
joshuaobare <jobare.obare@gmail.com>
2023-08-12 12:23:06 +0300

    added form to FullPost component

--- client/src/components/FullPost.js
+++ client/src/components/FullPost.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Comment from "./Comment"
+import Comment from "./Comment";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function FullPost(props) {
   const [postData, setPostData] = useState({});
-  const [postComments, setPostComments] = useState({});
+  const [postComments, setPostComments] = useState([]);
   const [postId, setPostId] = useState("");
   const { id } = useParams();
 
   const fetchPostData = async () => {
     const postResponse = await fetch(`http://localhost:3000/posts/post/${id}`);
     const post = await postResponse.json();
-    setPostData(post.post)
-    setPostComments(post.comments)
-    
+    setPostData(post.post);
+    setPostComments(post.comments);
   };
 
   useEffect(() => {
@@ -25,9 +26,19 @@ export default function FullPost(props) {
       <h1>{postData.title}</h1>
       <p>{postData.text}</p>
       <h3>Comments</h3>
-      {
-          postComments.map(comment => <Comment comment={comment}/>)
-      }
+      {postComments.map((comment) => (
+        <Comment key={comment._id} comment={comment} />
+      ))}
+      <form method="post">
+        <div className="form-group">
+          <label htmlFor="text">Text</label>
+          <textarea type="text" className="form-control text" name="text" id="text"></textarea>
+        </div>
+        <div className="form-group">
+            <button className="btn btn-primary">Submit</button>
+        </div>
+
+      </form>
     </div>
   );
 }
