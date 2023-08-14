c8dc2f836f72b301849ab90fc189594e2967e949
joshuaobare <jobare.obare@gmail.com>
2023-08-14 10:46:44 +0300

    updated userModel

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
 });
 
-module.exports = mongoose.model("User" , UserSchema)
+module.exports = mongoose.model("User", UserSchema);
