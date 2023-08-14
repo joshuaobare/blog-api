82413d2b597662115464b495d908338ed91ced8a
joshuaobare <jobare.obare@gmail.com>
2023-08-14 10:45:43 +0300

    updated userModel and cms css

--- cms/src/components/Login.js
+++ cms/src/components/Login.js
@@ -2,7 +2,7 @@ export default function Login() {
   return (
     <div className="login">
       <h1>Login Page</h1>
-      <form action="">
+      <form action="" className="login-form">
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <input type="email" id="email" className="form-control" name="email"/>
