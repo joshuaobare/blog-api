3ba4486bd33ae1216e32e71d3a77cba1a57d56a4
joshuaobare <jobare.obare@gmail.com>
2023-08-16 17:10:18 +0300

    updated both cms and client homepost components to display formatted dates

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -11,7 +11,7 @@ export default function Post(props) {
           </div>
           <Card.Subtitle className="mb-2 text-muted">
             <div>{props.post.authorId}</div>
-            <div>{props.post.timestamp}</div>
+            <div>{(new Date(props.post.timestamp)).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</div>
           </Card.Subtitle>
           <div className="homepage-post-text">
             <Card.Text>{props.post.text}</Card.Text>
