83bafa534a1be87cd784840e8282a2611212daea
joshuaobare <jobare.obare@gmail.com>
2023-08-16 13:37:45 +0300

    added loggedIn state to cms App.js and conditional rendering based on it

--- cms/src/App.js
+++ cms/src/App.js
@@ -1,20 +1,39 @@
-import './App.css';
-import Login from './components/Login'
-import SignUp from './components/SignUp'
-import CreatePost from './components/CreatePost'
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import CreatePost from "./components/CreatePost";
 import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  console.log(loggedIn);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userData = localStorage.getItem("user");
+
+    if (token !== null && userData !== null) {
+      setLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter basename='/admin'>
+      <BrowserRouter basename="/admin">
         <Routes>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/signup' element={<SignUp />}/>
-          <Route path='/post' element={<CreatePost />}/>
+          {!loggedIn ? (
+            <Route path="/login" element={<Login />} />
+          ) : (
+            <Route path="/" element={<Home />} />
+          )}
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/post" element={<CreatePost />} />
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
